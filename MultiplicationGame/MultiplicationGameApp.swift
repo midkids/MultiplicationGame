5d0ed0f56f2bab4b43c90e0985db80bfca5f24fe
midkids <83714759+midkids@users.noreply.github.com>
2026-04-17 21:13:49 -0400

    Initial Commit

---
+++ MultiplicationGame/MultiplicationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationGameApp.swift
+//  MultiplicationGame
+//
+//  Created by Myron Snelson on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
